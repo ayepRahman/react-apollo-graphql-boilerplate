66369ab0a2bd187e629bde4f383ad31be3329158
arifrahman <matrep88@gmail.com>
2019-12-24 23:23:00 +0800

    fixed server generator

--- server/src/features/rootModels.ts
+++ server/src/features/rootModels.ts
@@ -1,5 +1,7 @@
 import { Todos } from './todos/model';
+/* CODE-GENERATOR - ROOTMODELS IMPORT */
 
 export default {
   Todos,
+  /* CODE-GENERATOR - ROOTMODELS DEFAULT */
 };
--- server/src/features/rootSchema.ts
+++ server/src/features/rootSchema.ts
@@ -1,4 +1,10 @@
 import { mergeSchemas } from 'graphql-tools';
 import todosSchema from './todos/schema';
+/* CODE-GENERATOR - ROOTSCEHMA IMPORT */
 
-export default mergeSchemas({ schemas: [todosSchema] });
+export default mergeSchemas({
+  schemas: [
+    todosSchema,
+    /* CODE-GENERATOR - ROOTSCEHMA DEFAULT */
+  ],
+});
